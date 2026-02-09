19fb6b60b20de697125be410f18a43367ecb4c53
toneyb8038-create <toneyb8038@student.faytechcc.edu>
2026-02-09 10:35:23 -0500

    Delete P1LAB1.py

--- P1LAB1.py
+++
@@ -1,9 +0,0 @@
-# Brennan Toney
-# 9/5/2024
-# P1LAB1.py
-# This program will ask the user for their name and then greet them.
-
-
-name = input("What is your name? ")
-print(f"Hello {name}, welcome to CTI-110!")
-
