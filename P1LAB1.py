83f93b0dba223eb766c259c335272e9484fde2fd
toneyb8038-create <toneyb8038@student.faytechcc.edu>
2026-02-09 10:35:42 -0500

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,12 @@
+# Brennan Toney
+# 9/5/2024
+# P1LAB1.py
+# This program will ask the user for their name and then greet them.
+
+
+# Prompt for first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110!.")
